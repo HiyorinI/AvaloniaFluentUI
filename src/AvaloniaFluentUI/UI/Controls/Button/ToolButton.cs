c404b6cf5904452ff63763590f0255a8ba31de74
Izumi <hanserinterval@outlook.com>
2026-05-13 23:11:47 +0800

    添加工具按钮

---
+++ src/AvaloniaFluentUI/UI/Controls/Button/ToolButton.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia;
+using Avalonia.Media;
+
+namespace AvaloniaFluentUI.UI.Controls;
+
+public class ToolButton : Avalonia.Controls.Button
+{
+    public static readonly StyledProperty<Geometry> IconDataProperty =
+        AvaloniaProperty.Register<ToolButton, Geometry?>(nameof(IconData));
+
+    public Geometry? IconData
+    {
+        get => GetValue(IconDataProperty);
+        set => SetValue(IconDataProperty, value);
+    }
+}
